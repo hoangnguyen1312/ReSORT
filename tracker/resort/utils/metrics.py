aeccb4847a01424ef894e828b9c7fdba22508283
Tran Hoang Nguyen <nguyen.tran1312@hcmut.edu.vn>
2021-10-19 13:11:18 +0700

    update

--- tracker/resort/utils/metrics.py
+++ tracker/resort/utils/metrics.py
@@ -3,20 +3,20 @@ import os
 import numpy as np
 
 def load_features(src):
-    print("[+] Load features....")
+    # print("[+] Load features....")
     data = []
     label = []
     files = [f for f in os.listdir(src) if os.path.isfile(os.path.join(src, f)) and f.endswith('.npy')]
     for i, f in enumerate(files):
         data.append(np.load(os.path.join(src, f)))
         label.append(f.split(".")[0])
-    print("[+] Load features finished")
+    # print("[+] Load features finished")
     return np.array(data), np.array(label)
 
 
 def save_feature(save_path, feature):    
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    print("[+]Save extracted feature to file : ", save_path)
+    # print("[+]Save extracted feature to file : ", save_path)
     np.save(save_path, feature[0,:])
 
 
@@ -26,7 +26,7 @@ def calculate_similarity_euclid(thumb, src, tolerance=1.16):
         return -1, -1
     preds = np.linalg.norm(features - thumb, axis=1)
     sort_prob = np.argsort(preds)
-    print('===================>', preds)
+    # print('===================>', preds)
     # print(preds)
     # print('------------->', preds[sort_prob[0]])
     if preds[sort_prob[0]] > tolerance:
@@ -45,7 +45,7 @@ def calculate_similarity_cosine(thumb, src, tolerance=0.44):
         return -1, -1
     preds = cosin_metric(features, thumb[0])
     sort_prob = np.argsort(preds)
-    print('===================>', preds)
+    # print('===================>', preds)
     if preds[sort_prob[-1]] < tolerance:
         return -1, -2
     return int(labels[sort_prob[-1]]), preds[sort_prob[-1]]
